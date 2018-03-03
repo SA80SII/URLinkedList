be6904948966a6109ed7ea0ec79aa3a32f3777ef
Wes <wlewis5@u.rochester.edu>
2018-03-03 02:34:33 -0500

    ok

--- TestClass.java
+++ TestClass.java
@@ -4,9 +4,8 @@ public class TestClass {
 	public static <E> void main(String[] args) {
 		URLinkedList<Integer> list = new URLinkedList<Integer>();
 		list.add(0,4);
-		System.out.println(list.get(0));
-		list.addFirst(0);
-		System.out.println(list.get(0));
+		System.out.println(list.size());
+	
 			}
 
 }
